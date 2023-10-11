5781bde8744e9db4b2dff899d0b7ec31fa853779
ismael hernan uriarte <ismaelu064@gmail.com>
2023-10-11 10:48:41 -0300

    rehacer todo porque lo que hice era incorrecto

--- script.js
+++ script.js
@@ -1,130 +1,89 @@
-//Bienvenida a nuestra dietetica, el usuario debe ingresar su nombre y apellido obligatoriamente para poder realizar su selección de productos
-alert("¡Bienvenidos a la dietética los hermanos, selecciones sus productos!");
 
-let nombreIngresado   = "";
-let apellidoIngresado = "";
-
-while (nombreIngresado ==="" || apellidoIngresado ==="") {
-    nombreIngresado = prompt("Ingresar nombre"); 
-    apellidoIngresado = prompt("Ingresar apellido");
-
-    if (nombreIngresado ==="" || apellidoIngresado ==="") {
-        alert("Error: Ingresar nombre y apellido.")
-    }else{
-        alert("Bienvenido "+nombreIngresado + " " + apellidoIngresado);
-    }
-}
-// productos dieteticos, vende mix dieteticos por kilo, el cliente pone cantidad, marca seleccionada, da como resultados valores a pagar y costo de envio calculuado por cantidad del producto seleccionado.
-// variables globales
-const mixdefrutosA = 3300
-const mixdefrutosB = 2200
-const mixdefrutosC = 1100
-
-let cantidad; 
-let marcaAlimento;
-let resultado;
-
-//operacion multiplicar
-function multiplicar(valor1, valor2){
-    const resultado = valor1 * valor2;
-    return resultado;
-}
-//paso 2: que nos diga por prompt la cantidad de kilos de alimento para mascota que quiere llevar, teniendo en cuenta que 0. cierra el ciclo.
-//paso 3: elija la marca de alimento que desee comprar, con A B y C.
-
-while (cantidad != 0){
-    cantidad = parseInt(prompt("Ingrese la cantidad de mix que desea llevar! Para terminar presione 0"));
-    
-    if (cantidad === 0){
-        break;
-    }
-
-    marcaAlimento = prompt("Seleccione el mix que mas le guste:\n\ -Si quiere mix-de frutos secos Premium ' Ingrese ' = 1 \n\ -Si quiere mix-de frutos secos Tropicales ' Ingrese ' = 2 \n\ -Si quiere mix-de frutos secos Económico ' Ingrese ' = 3");
-
-    switch (marcaAlimento){
-
-        case "1":
-        resultado = multiplicar(mixdefrutosA, cantidad);
-        alert("Debe abonar $ "+ resultado + " por su compra de mix-de frutos secos Premium");
-        break;
-
-        case "2":
-            resultado = multiplicar(mixdefrutosB, cantidad);
-            alert("Debe abonar $ "+ resultado + " por su compra de mix-de frutos secos Tropicales");
-            break;
-
-        case "3":
-            resultado = multiplicar(mixdefrutosC, cantidad);
-            alert("Debe abonar $ "+ resultado+ " por su compra de mix-de frutos secos Económico");
-            break;
-
-        default:
-            alert("Operación Invalida");
-            break;
-    }
-
-    if (cantidad >= 10 ){
-        valorEnvio = 0
-        alert("Su costo de envio es $"+ valorEnvio);
-    }
-    else if (cantidad >=5 ){
-        valorEnvio = 200
-        alert("Su costo de envio es $"+ valorEnvio);
-    }
-    else {
-        valorEnvio = 250
-        alert("Su costo de envio es $"+ valorEnvio);
-    }
-}
-alert("Gracias por su compra")
-
-
-let products = [
-    "Mix-tropical",
-    "Mix-premium",
-    "Pasas de uva",
-    "Almendras",
-    "Nueces",
-    "Arroz integral",
-    "Bebida energetica",
-    "Castañas",
-    "Yerba organica",
-    "Proteinas organicas"
+// Array de productos
+const productos = [
+    { nombre: "Manzana", categoria: "Frutas" },
+    { nombre: "Leche", categoria: "Lácteos" },
+    { nombre: "Pasta", categoria: "Alimentos secos" },
+    { nombre: "Zanahoria", categoria: "Verduras" },
+    // Agrega más productos según sea necesario
 ];
 
-let sortedProducts = products.sort();
+  // Función para buscar productos por nombre
+function buscarProductoPorNombre(nombre) {
+    const resultados = [];
+    
 
-let input = document.getElementById("input");
+for (const productos of productos) {
+    
+    
+if (productos.nombre.toLowerCase().includes(nombre.toLowerCase())) {
+        resultados.
+        resultados
 
+    
+push(productos);
+    }
+    }
+    return resultados;
+}
 
-input.addEventListener("keyup", (e) => {
-    removeElements();
-    for(let i of sortedProducts){
-        if(i.toLowerCase() .startsWith(input.value.toLowerCase()) && input.value != ""){
-            let listItem = document.createElement("li");
-            listItem.classList.add("list-items");
-            listItem.style.cursor = "pointer";
-            listItem.setAttribute("oneclick", "displayProducts('" + i + "')");
+  // Función para filtrar productos por categoría
+function filtrarProductosPorCategoria(categoria) {
+    return productos.filter(producto => producto.categoria === categoria);
+}
 
-            let word = "<b>" + i.substring(0,input.value.length) + "</b>";
-            word += i.substring(input.value.length);
+  // Ejemplo de uso
+alert("¡Bienvenidos a la dietética los hermanos, seleccione sus productos!");
 
-            listItem.innerHTML = word;
-            document.querySelector(".list").appendChild(listItem)
+alert("Ingrese sus datos para continuar");
+
+let nombreIngresado = "";
+let apellidoIngresado = "";
+
+while (nombreIngresado === "" || apellidoIngresado === "") {
+    nombreIngresado = 
+    nombreIngresado =
+prompt("Ingresar nombre");
+    apellidoIngresado = 
+    apellidoIngresado =
+prompt("Ingresar apellido");
+
+    if (nombreIngresado === "" || apellidoIngresado === "") {
+    alert("Error: Ingrese nombre y apellido.");
+    } else {
+        alert
+        alert("Bienvenido " + nombreIngresado + " " + apellidoIngresado);
+
+            const busqueda = prompt("¿Qué producto desea buscar por nombre? (Deje en blanco si no desea buscar)");
+            const categoria = prompt("¿Desea filtrar por categoría? (Deje en blanco si no desea filtrar)");
+
+            if (busqueda) {
+                const resultadosBusqueda = buscarProductoPorNombre(busqueda);
+                alert("Resultados de la búsqueda:");
+                console.log(resultadosBusqueda);
+            }
+
+            if (categoria) {
+                const productosFiltrados = filtrarProductosPorCategoria(categoria);
+                alert("Productos en la categoría " + categoria + ":");
+                console.log(productosFiltrados);
+            }
         }
     }
-});
 
-function displayProducts(value){
-    input.value = value;
-}
 
-function removeElements(){
-    let items = document.querySelectorAll(".list-items");
-    items.forEach((items) =>{
-        items.remove();
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
